6c60d70db00bc0b3cb448545df36e96ce45c0928
Carlos Puentestar <puentestarchamorrocarlosdaniel@gmail.com>
2024-05-01 12:21:28 -0500

    Recuperar código

--- Notaciones.cs
+++ Notaciones.cs
@@ -17,7 +17,7 @@ namespace ClaseNotaciones
 
         public NInfija(string notacion)
         {
-            this.notacion = notacion;csdd
+            this.notacion = notacion;
         }
 
         public NPosfija APosfija()
